8a806dcabdaa3c7eca96347dedbc97cbcacd435b
apple <apple@appledemacbook-pro.local>
2016-07-19 14:31:35 +0800

    Initial Commit

---
+++ ViewControllerTransAnimate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewControllerTransAnimate
+//
+//  Created by apple on 7/19/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewControllerTransAnimate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ViewControllerTransAnimate
+//
+//  Created by apple on 7/19/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
